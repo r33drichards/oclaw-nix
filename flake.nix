c288856d7fb2bf7dfc8ea7241bcde80c71f19550
r33drichards <rwendt1337@gmail.com>
2026-03-24 10:52:32 -0700

    Update hypervisor SSH authorized key

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
           users.users.root.openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJNEMM9i3WgPeA5dDmU7KMWTCcwLLi4EWfX8CKXuK7s robertwendt@Roberts-Laptop.local"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlI6KJHGNUzVJV/OpBQPrcXQkYylvhoM3XvWJI1/tiZ"
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4mlN4JTkdx3C7iBmMF5HporlQygDE2tjN77IE0Ezxn root@hypervisor"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKBlj6rlbIbhrnBIGBx7Kg5lCFcG5Kx7IdoCxCLpoSGF root@hypervisor"
           ];
 
           nix.settings.experimental-features = [ "nix-command" "flakes" ];
